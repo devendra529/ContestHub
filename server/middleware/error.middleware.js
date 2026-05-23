c5d92546d064cdab83e59b16bfeff8150a4b9aa7
devendra529 <singhdevendrapratap529@gmail.com>
2026-05-23 14:52:30 +0530

    adding the error.middleware

---
+++ server/middleware/error.middleware.js
@@ -0,0 +1,46 @@
+// server/middleware/error.middleware.js
+const ApiError = require("../utils/ApiError");
+
+const errorMiddleware = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message    = err.message    || "Internal Server Error";
+
+  if (err.name === "CastError") {
+    statusCode = 400;      //this uausally happens when an invalidMongodb objectId id is provided
+    message    = `Invalid ${err.path}: ${err.value}`;
+  }
+
+  if (err.name === "ValidationError") {
+    statusCode = 400;  //this happens when  a mongoose schema validation fails
+    message    = Object.values(err.errors).map((val) => val.message).join(", ");
+  }
+
+  if (err.code === 11000) {
+    statusCode = 409;
+    const field = Object.keys(err.keyValue)[0];
+    message    = `${field} already exists`;
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    statusCode = 401;
+    message    = "Invalid token. Please login again";
+  }
+
+  if (err.name === "TokenExpiredError") {
+    statusCode = 401;
+    message    = "Token expired. Please login again";
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    console.error("ERROR:", err);
+  }
+//always send a json response with success:false and the error message. 
+  res.status(statusCode).json({
+    success    : false,
+    statusCode : statusCode,
+    message    : message,
+    ...(process.env.NODE_ENV === "development" && { stack: err.stack }),
+  });
+};
+
+module.exports = errorMiddleware;
